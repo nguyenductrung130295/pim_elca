c044c82f75479d662b1aa49960051d4a8eef69a0
Nguyen Duc Trung <trungnguyencntt1302@gmail.com>
2018-11-26 17:56:32 +0700

    config multilingual

--- src/main/java/vn/elca/training/PimElcaApplication.java
+++ src/main/java/vn/elca/training/PimElcaApplication.java
@@ -1,12 +1,38 @@
 package vn.elca.training;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
-public class PimElcaApplication {
+public class PimElcaApplication implements WebMvcConfigurer {
+    public static void main(String[] args) {
+        SpringApplication.run(PimElcaApplication.class, args);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(PimElcaApplication.class, args);
-	}
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.US);
+        return localeResolver;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        return localeChangeInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
 }
---
+++ src/main/resources/static/js/myjavascript.js
@@ -0,0 +1,4 @@
+$("#fr_lang").click(function(){
+	console.log("french");
+	window.location.replace('?lang=vi');
+})
