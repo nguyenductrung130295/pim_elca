f85d4e3234f6d3b62e6650338f8f1ef55aa2c3b4
Nguyen Duc Trung <trungnguyencntt1302@gmail.com>
2018-11-26 15:22:30 +0700

    front end done with out multi language

---
+++ src/main/java/vn/elca/training/entities/Project.java
@@ -0,0 +1,25 @@
+package vn.elca.training.entities;
+
+public class Project {
+    private Long id;
+    private String name;
+
+    public Project() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- src/main/java/vn/elca/training/web/PimController.java
+++ src/main/java/vn/elca/training/web/PimController.java
@@ -1,14 +1,30 @@
 package vn.elca.training.web;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import vn.elca.training.entities.Project;
 
 @Controller("project")
+@RequestMapping("/project")
 public class PimController {
     @RequestMapping("/")
     @ResponseBody
     String test() {
         return "lkasjdlkfas";
+    }
+
+    @RequestMapping("/new")
+    ModelAndView newProject() {
+        return new ModelAndView("new", "project", new Project());
+    }
+
+    @RequestMapping("/list")
+    ModelAndView listProject() {
+        return new ModelAndView("list", "projectList", new ArrayList<Project>());
     }
 }
